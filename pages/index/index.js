ef17cd547ddcc17d648c9a358cf41f2b98413a78
GaoDasheng <gaodas@126.com>
2018-07-02 13:51:49 +0800

    启动页添加链接

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,60 +6,15 @@ Page({
   data: {
     
   },
+  start: function(){
+    //跳转到子页面，子页面自动添加返回按钮
+    //  wx.navigateTo({
+    //    url: '/pages/posts/post',
+    //  })
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+    //跳转到平行页面
+    wx.redirectTo({
+      url: '/pages/posts/post',
+    })
   }
 })
